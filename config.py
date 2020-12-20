0cba44f2dbc8e5aa5db97b06f2ab42ffce61760d
hazim111 <hazimmir006@gmail.com>
2020-12-20 16:59:58 +0100

    commit 7

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ TRAIN_BATCH_SIZE = 8
 VALID_BATCH_SIZE = 4
 EPOCHS = 1
 ACCUMULATION = 2
-BERT_PATH = "s3://static-nlp-data/bert-base-uncased/"
+BERT_PATH = "s3://static-nlp-data/bert-base-uncased"
 MODEL_PATH = "s3://static-nlp-data/model/model.bin"
 TRAINING_FILE = "IMDB Dataset.csv"
 TOKENIZER = transformers.BertTokenizer.from_pretrained(BERT_PATH, do_lower_case=True)
